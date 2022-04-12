8f178e45e93b7fbfca41fc8107567e2b4438093a
Daniel Holth <dholth@fastmail.fm>
2022-04-11 20:57:28 -0400

    exclude extra files from series; update jpatchset

--- app/patchme.py
+++ app/patchme.py
@@ -3,11 +3,13 @@
 
 import glob
 import subprocess
-import tempfile
 import json
 import os.path
 import pathlib
 import logging
+import re
+
+import truncateable
 
 # if numbers get very large,
 # import natsort
@@ -18,21 +20,18 @@ log = logging.getLogger(__name__)
 cmd = "/jpatchset"
 
 
+def is_series(filename):
+    return re.match("\w*(-\d+)?\.json", filename)
+
+
 def patchme():
     """
     chdir to desired directory, then call.
     """
     current_repodata = list(
-        filter(
-            lambda x: not x.endswith("-patch.json"),
-            sorted(glob.glob("current_repodata*.json")),
-        )
+        filter(is_series, sorted(glob.glob("current_repodata*.json")),)
     )
-    repodata = list(
-        filter(
-            lambda x: not x.endswith("-patch.json"), sorted(glob.glob("repodata*.json"))
-        )
-    )
+    repodata = list(filter(is_series, sorted(glob.glob("repodata*.json"))))
 
     for patchset, series in (
         ("current_repodata-patch.json", current_repodata),
@@ -63,3 +62,17 @@ def patchme():
                 print(os.getcwd(), e)
                 continue
             latest_path.write_text(left)
+
+        data = json.load(open(patchset, "r"))
+        with open(pathlib.Path(patchset).with_suffix(".jlap"), "wb+") as out:
+            writer = truncateable.JlapWriter(out)
+            for obj in reversed(data["patches"]):
+                writer.write(obj)
+            data.pop("patches")
+            headers = pathlib.Path(series[-1]).with_stem(
+                pathlib.Path(series[-1]).stem + "-headers"
+            )
+            if headers.exists():
+                data["headers"] = json.loads(headers.read_text())
+            writer.write(data)
+            writer.finish()
