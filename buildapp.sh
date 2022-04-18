628c14849aa3e17dc9cd38dff325d40990bba682
Daniel Holth <dholth@fastmail.fm>
2022-04-18 07:58:45 -0400

    more action

--- buildapp.sh
+++ buildapp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd app
 # pypy package 'zipapps' makes self-contained file
-python -m zipapps -c -m repodata_proxy:go -a repodata_proxy.py,no_cache.py,sync_jlap.py,truncateable.py,update_conda_cache.py -r ../requirements.txt -o ../repodata.pyz
+python -m zipapps -p /usr/bin/python3 -c -m repodata_proxy:go -a repodata_proxy.py,no_cache.py,sync_jlap.py,truncateable.py,update_conda_cache.py -r ../requirements.txt -o ../repodata.pyz
 chmod +x ../repodata.pyz
