e80224a1a2ab4b86e893e733f55fdabaf9aea574
Daniel Holth <dholth@fastmail.fm>
2022-06-06 17:29:27 -0400

    update buildapp.sh to build second tool

--- buildapp.sh
+++ buildapp.sh
@@ -3,3 +3,7 @@ cd app
 # pypy package 'zipapps' makes self-contained file
 python -m zipapps -p /usr/bin/python3 -c -m repodata_proxy:go -a repodata_proxy.py,no_cache.py,sync_jlap.py,truncateable.py,update_conda_cache.py -r ../requirements.txt -o ../repodata.pyz
 chmod +x ../repodata.pyz
+
+# standalone json-to-jlap
+python -m zipapps -p /usr/bin/python3 -c -m json2jlap:go -a json2jlap.py,truncateable.py -r ../requirements-json2jlap.txt -o ../json2jlap.pyz
+chmod +x ../json2jlap.pyz
